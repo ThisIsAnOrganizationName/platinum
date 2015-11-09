feb80f60d9ec6bfbc3eac599e58632792f645f40
yneear <yneear@gmail.com>
2015-11-09 23:57:53 +0800

    router

---
+++ mobile/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="wrapper">
+        <h1>hello world</h1>
+        <router-view keep-alive transition>
+        </router-view>
+    </div>
+</template>
+<script>
+</script>
